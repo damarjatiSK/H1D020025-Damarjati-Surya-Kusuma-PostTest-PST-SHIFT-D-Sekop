8894f13fba60eb779443f7abd68435fc667935ba
Damarjati Surya Kusuma <dmrjtsk363@gmail.com>
2024-05-20 16:02:38 +0700

    post test

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 // Definisikan pin untuk lampu merah dan hijau
-const int redLightPin = 8;
-const int greenLightPin = 9;
+const int lampuMerah = 8;
+const int lampuHiaju = 9;
 
 // Waktu nyala masing-masing lampu dalam milidetik
 const unsigned long interval = 2000;
@@ -15,23 +15,23 @@ const unsigned long interval = 2000;
 void setup()
 {
   // Atur pin sebagai output
-  pinMode(redLightPin, OUTPUT);
-  pinMode(greenLightPin, OUTPUT);
+  pinMode(lampuMerah, OUTPUT);
+  pinMode(lampuHiaju, OUTPUT);
 
   // Inisialisasi LCD
   lcd.begin();
   lcd.backlight();
 
   // Buat semua lampu mati di awal
-  digitalWrite(redLightPin, LOW);
-  digitalWrite(greenLightPin, LOW);
+  digitalWrite(lampuMerah, LOW);
+  digitalWrite(lampuHiaju, LOW);
 }
 
 void loop()
 {
   // Nyalakan lampu merah dan matikan lampu hijau
-  digitalWrite(redLightPin, HIGH);
-  digitalWrite(greenLightPin, LOW);
+  digitalWrite(lampuMerah, HIGH);
+  digitalWrite(lampuHiaju, LOW);
   // Tampilkan pesan di LCD
   lcd.clear();
   lcd.print("Lampu merah menyala");
@@ -39,8 +39,8 @@ void loop()
   delay(interval);
 
   // Matikan lampu merah dan nyalakan lampu hijau
-  digitalWrite(redLightPin, LOW);
-  digitalWrite(greenLightPin, HIGH);
+  digitalWrite(lampuMerah, LOW);
+  digitalWrite(lampuHiaju, HIGH);
   // Tampilkan pesan di LCD
   lcd.clear();
   lcd.print("Lampu hijau menyala");
